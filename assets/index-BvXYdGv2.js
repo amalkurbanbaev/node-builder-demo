f0bd70706af19ced87b0c9e132839134be424815
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-03-10 19:01:59 +0500

    Updates

---
+++ assets/index-BvXYdGv2.js
@@ -0,0 +1,1 @@
+import{j as t,N as o}from"./index-COjP6dgS.js";const s=function(){return t.jsx(o,{to:"/settings/general"})};export{s as component};
--- assets/index-CxhAUPzD.js
+++
@@ -1,1 +0,0 @@
-import{o as r,p as o}from"./index-EEqrqf0P.js";var t=o();const e=r(t);export{e as R,t as r};
